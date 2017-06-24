ddb06f90631b56c204c419f4fa676aef78f9697d
Tanawat Tassana <amp.tanawat@gmail.com>
2017-06-25 01:35:14 +0700

    Add book to record on firebase

--- src/app/state/ducks/record/sagas.js
+++ src/app/state/ducks/record/sagas.js
@@ -6,7 +6,9 @@ import {
   saveRecordToLocal,
   fetchRecordsFromLocal,
   saveBookToLocal,
-  fetchRecords as fetchRecordsFromRemote
+  fetchRecords as fetchRecordsFromRemote,
+  updateRecords,
+  updateBooks
 } from '../../../api/record'
 import {lens as sessionLens} from '../session'
 
@@ -18,8 +20,15 @@ function* saveRecord() {
       date: recordDate.format('YYYY-MM-DD'),
       read: true
     }
-    const records = yield call(saveRecordToLocal, record)
-    yield put(actions.recordsChange(records))
+    const user = yield select(state => R.view(sessionLens.userLens, state.session))
+    if (user) {
+      const records = {[record.data]: record}
+      // TODO: save to remote server
+      //yield call(updateRecords, records)
+    } else {
+      const records = yield call(saveRecordToLocal, record)
+      yield put(actions.recordsChange(records))
+    }
   }
 }
 
@@ -42,12 +51,25 @@ function* watchFetchRecords() {
 function* addBookToRecord() {
   while (true) {
     const {payload: {record, book, pages}} = yield take(types.ADD_BOOK_TO_RECORD)
-    yield call(saveBookToLocal, book)
-    const newRecord = R.merge(record, {
-      books: R.append({title: book.title, pages}, record.books || [])
-    })
-    const records = yield call(saveRecordToLocal, newRecord)
-    yield put(actions.recordsChange(records))
+    const user = yield select(state => R.view(sessionLens.userLens, state.session))
+    if (user) {
+      const books = {[book.title]: book}
+      yield call(updateBooks, books)
+      const newRecord = R.merge(record, {
+        books: R.append({title: book.title, pages}, record.books || [])
+      })
+      const records = {[newRecord.date]: newRecord}
+      yield call(updateRecords, user, records)
+      const remoteRecords = yield call(fetchRecordsFromRemote, user)
+      yield put(actions.recordsChange(remoteRecords))
+    } else {
+      yield call(saveBookToLocal, book)
+      const newRecord = R.merge(record, {
+        books: R.append({title: book.title, pages}, record.books || [])
+      })
+      const records = yield call(saveRecordToLocal, newRecord)
+      yield put(actions.recordsChange(records))
+    }
   }
 }
 
