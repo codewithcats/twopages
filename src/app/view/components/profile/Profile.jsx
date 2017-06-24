81237e30614e570ff8efeaa14ec5b933c924e182
Tanawat Tassana <amp.tanawat@gmail.com>
2017-06-24 16:28:41 +0700

    Layout registration form

--- src/app/view/components/profile/Profile.jsx
+++ src/app/view/components/profile/Profile.jsx
@@ -1,35 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const ActionContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center; 
-`
+import RegistrationForm from './RegistrationForm'
 
-const Or = styled.h4`
-  margin: 0 1rem !important;
-`
-
-const UnlockTitle = styled.h5`
-  text-align: center;
-  margin-top: 2rem;
-`
 
 const Profile = () => {
   return (
     <section>
-      <ActionContainer>
-        <button className="button is-medium is-primary">Register</button>
-        <Or className="title is-4">or</Or> 
-        <button className="button is-medium is-primary">Sign In</button>
-      </ActionContainer>
-      <UnlockTitle className="title is-5">
-        To Unlock Full Features
-      </UnlockTitle>
-      <div>
-        
-      </div>
+      <RegistrationForm />
     </section>
   )
 }
